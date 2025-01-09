1e4ff35c60ad0cf8380a6f7d77eda5e076e17026
unknown <karkisugam34@gmail.com>
2025-01-09 11:33:29 +0545

    js

---
+++ 53.js
@@ -0,0 +1,20 @@
+console.log("Este es tutorial numbero 53");
+
+function introduce(name){
+    console.log(name + " es un hombre.");
+}
+
+function sum(a, b, c){
+    return (a+b+c);
+}
+
+let name1="Jose";
+let name2="Pedro";
+let name3="Rakitic";
+
+introduce(name1);
+introduce(name2);
+introduce(name3);
+
+let val=sum(1,2,3);
+console.log(val);
